43a238213c34aba1ef7bca26b09682975e1195fa
mightykukulkan <uma.s.shankar@gmail.com>
2019-04-16 13:04:01 +0530

    my ann

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/myfirstpy.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/myfirstpy.py
@@ -53,9 +53,12 @@ cm = confusion_matrix(y_test, y_pred)
 #predicting a single function
 y_pred_single = classifier.predict(standardScaler.transform(np.array([[0.0,0,600, 1, 40, 3, 60000, 2,  1, 1, 50000]])))
 
+import sklearn
+print('The scikit-learn version is {}.'.format(sklearn.__version__))
 
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score 
+from sklearn.model_selection import GridSearchCV
 def build_classifier():    
     classifier  = Sequential()
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
@@ -65,4 +68,30 @@ def build_classifier():
     return classifier 
 
 classifier = KerasClassifier(build_fn = build_classifier,  batch_size=10, epochs=100)
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1) 
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = 1) 
+mean = accuracies.mean()
+variance = accuracies.std()
+
+from keras.wrappers.scikit_learn import KerasClassifier 
+from sklearn.model_selection import GridSearchCV
+def build_classifier(optimizer):    
+    classifier  = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier 
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size' : [25, 32],
+              'epochs':[100, 500],
+              'optimizer': ['adam', 'rmsprop']}
+
+grid_search = GridSearchCV(estimator = classifier, 
+                           param_grid = parameters,
+                           scoring='accuracy',
+                           cv=10) 
+grid_search = grid_search.fit(X_train, y_train)
+best_params = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+#parameter tuning
+

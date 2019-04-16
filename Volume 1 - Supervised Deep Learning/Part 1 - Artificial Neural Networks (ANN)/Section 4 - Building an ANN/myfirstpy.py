da9fce7eef35665dea39a3f81337bf49959ffba2
mightykukulkan <uma.s.shankar@gmail.com>
2019-04-16 12:47:31 +0530

    my first commit

---
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/myfirstpy.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+
+features = dataset.iloc[:, 3:13].values
+label = dataset.iloc[:, 13].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_country = LabelEncoder()
+features[:, 1] = labelEncoder_country.fit_transform(features[:, 1])
+labelEncoder_gender = LabelEncoder()
+features[:, 2] = labelEncoder_gender.fit_transform(features[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+features = onehotencoder.fit_transform(features).toarray()
+features  = features[:, 1:] #removing one coloumn to avoid dummy variable trap 
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(features, label, test_size = 0.2, random_state = 0)
+
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+standardScaler = StandardScaler()
+X_train =standardScaler.fit_transform(X_train)
+X_test  = standardScaler.transform(X_test)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier  = Sequential()
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
+
+y_pred = classifier.predict(X_test)
+
+y_pred = (y_pred > 0.5)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#predicting a single function
+y_pred_single = classifier.predict(standardScaler.transform(np.array([[0.0,0,600, 1, 40, 3, 60000, 2,  1, 1, 50000]])))
+
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score 
+def build_classifier():    
+    classifier  = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier 
+
+classifier = KerasClassifier(build_fn = build_classifier,  batch_size=10, epochs=100)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1) 

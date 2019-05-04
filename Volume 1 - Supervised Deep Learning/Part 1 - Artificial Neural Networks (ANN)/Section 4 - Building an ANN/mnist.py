bddb3f21617a9d80e085f61937791bf90e2c0c65
mightykukulkan <uma.s.shankar@gmail.com>
2019-05-04 14:30:14 +0530

    mnist database

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/mnist.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/mnist.py
@@ -7,13 +7,17 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.preprocessing import LabelEncoder
 
 sc = StandardScaler()
 
 dataset = pd.read_csv('digit-recognizer/train.csv')
 x_train = dataset.iloc[:, 1:785].values
+x_train = sc.fit_transform(x_train)
+
+numberEncoder = LabelEncoder()
 y_train = dataset.iloc[:, 0].values
+y_train = numberEncoder.fit_transform(y_train)
 #y_train = sc.fit_transform(y_train)
 
 testset = pd.read_csv('digit-recognizer/test.csv')
@@ -32,14 +36,17 @@ classifier.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'r
 classifier.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu'))
 
 # Adding the output layer
-classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'softmax'))
 
 # Compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['sparse_categorical_accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(x_train, y_train, batch_size = 10, epochs = 100)
+classifier.fit(x_train, y_train, batch_size = 32, epochs = 100)
 
-y_pred = classifier.predict(np.array([x_test[3]]))
-print(y_pred)
+classifier.save('mnist.h5')
+
+y_pred = classifier.predict(x_train)
+y_pred = classifier.predict(np.array([x_train[3]]))
+print(y_pred[0].tolist().index(True))
 
